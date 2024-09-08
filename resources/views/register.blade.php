4f35541d5fba063c011bec97e48509273a34b3c7
Alpha-Interns <dalmaskahiu@gmail.com>
2024-09-08 23:26:42 +0300

    Create register.blade.php
    photographer registering form

---
+++ resources/views/register.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <label for="role">Role</label>
+    <select name="role" id="role" required>
+        <option value="photographer">Photographer</option>
+        <option value="viewer">Viewer</option>
+    </select>
+</div>
